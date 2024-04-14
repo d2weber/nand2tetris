b8447cbb039bebef153ef44ad7478fa9ff782088
d2weber <29163905+d2weber@users.noreply.github.com>
2024-04-14 13:42:07 +0200

    improve test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,11 @@
 #!/bin/env bash
-find . -iname "*.tst" -exec bash ../../tools/HardwareSimulator.sh {} \;
+
+if [ "$#" -gt 1 ]; then
+  echo "At most one argument allowed" >&2
+  exit 2
+fi
+
+HERE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TEST_DIR=${1:-"."}
+
+find "${TEST_DIR}" -iname "*.tst" -exec bash "${HERE}/../tools/HardwareSimulator.sh" {} \;
