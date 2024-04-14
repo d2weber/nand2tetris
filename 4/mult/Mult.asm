0034e1e5b89894519b920ff80aa36efc0c865be8
d2weber <29163905+d2weber@users.noreply.github.com>
2024-04-14 10:01:05 +0200

    p4: complete mult

--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -1,10 +1,39 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/4/Mult.asm
+//; This file is part of www.nand2tetris.org
+//; and the book "The Elements of Computing Systems"
+//; by Nisan and Schocken, MIT Press.
+//; File name: projects/4/Mult.asm
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-// The algorithm is based on repetitive addition.
+//; Multiplies R0 and R1 and stores the result in R2.
+//; (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//; The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+
+//; Init R2
+@0
+D=A
+@R2
+M=D
+
+//; Perform R0 accumulations of R1
+(LOOP)
+
+//; Jump to end if R0==0; --R0
+@R0
+D=M
+M=M-1
+@END
+D;JEQ
+
+//; R2+=R1
+@R1
+D=M
+@R2
+M=D+M
+
+@LOOP
+0;JMP
+
+
+(END)
+@END
+0;JMP
